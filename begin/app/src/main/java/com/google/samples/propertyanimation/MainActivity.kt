9b104beff66946fc17e08f8f3b57e4089f31e408
ababenko1 <ababenko@griddynamics.com>
2023-09-04 17:42:37 +0200

    Added translating

--- begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -76,18 +76,16 @@ class MainActivity : AppCompatActivity() {
     private fun rotater() {
         val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
         animator.duration = 1000
-        animator.addListener(object: AnimatorListenerAdapter(){
-            override fun onAnimationStart(animation: Animator?) {
-                rotateButton.isEnabled = false
-            }
-            override fun onAnimationEnd(animation: Animator?) {
-                rotateButton.isEnabled = true
-            }
-        })
+        disableViewDuringAnimation(rotateButton, animator)
         animator.start()
     }
 
     private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        disableViewDuringAnimation(translateButton, animator)
+        animator.start()
     }
 
     private fun scaler() {
@@ -102,4 +100,15 @@ class MainActivity : AppCompatActivity() {
     private fun shower() {
     }
 
+    private fun disableViewDuringAnimation(view: View,
+                                           animator: ObjectAnimator) {
+        animator.addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
+    }
 }
